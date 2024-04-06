7fec4dceda80336049a01e57ae1658ce668a8a4b
Dhruv Patel <122099280+dhruvurgr8@users.noreply.github.com>
2024-04-06 11:50:20 +0000

    first commmit

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,21 @@
 
-import React from "react";
+import React, { useEffect } from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const[data,setData] = useState(null);
+  useEffect(()=>{
+    (async function(){
+      const response = await fetch("https://dummyjson.com/products");
+      const dataObtained = await response.json();
+      setData(dataObtained);
+    })
+  },[])
+  var formattedJson = JSON.stringify(data, null, 2);
   return (
     <div>
         {/* Do not remove the main div */}
+        <pre>{formattedJson}</pre>
     </div>
   )
 }
