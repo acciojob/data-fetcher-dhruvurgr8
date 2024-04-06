9daeffe353df124818df1d039f4cc2ca5ea2e3b2
Dhruv Patel <122099280+dhruvurgr8@users.noreply.github.com>
2024-04-06 11:56:07 +0000

    error fixed

--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,29 @@
 
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
   const[data,setData] = useState(null);
+  const[error,setError] = useState(null);
   useEffect(()=>{
-    (async function(){
-      const response = await fetch("https://dummyjson.com/products");
-      const dataObtained = await response.json();
-      setData(dataObtained);
-    })
+    try{
+      (async function(){
+        const response = await fetch("https://dummyjson.com/products");
+        const dataObtained = await response.json();
+        setData(dataObtained);
+      })();
+    }
+    catch(error){
+      setError(error);
+    }
+    
   },[])
   var formattedJson = JSON.stringify(data, null, 2);
   return (
     <div>
         {/* Do not remove the main div */}
-        <pre>{formattedJson}</pre>
+        {error?<p>error</p>:<pre>{formattedJson}</pre>}
+        
     </div>
   )
 }
